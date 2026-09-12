5112e721c996e2091a3c5ff50d76e64a1c9143be
graysandwich <timyang.vancity@gmail.com>
2026-09-11 22:12:04 -0700

    multiplayer lag fix hopefully

--- docs/multiplayerStuff.js
+++ docs/multiplayerStuff.js
@@ -3,7 +3,7 @@
 const SERVER_URL =
   location.hostname === "localhost" || location.hostname === "127.0.0.1"
     ? "http://localhost:8080"
-    : "https://ewow-score-checker-698852688376.us-central1.run.app";
+    : "https://ewow-score-checker-698852688376.us-west1.run.app";
 let serverState = { code: "", players: {}, bullets: [], enemies: [], floatingObjects: [], enemyBullets: [], abilityIcons: [], mapObjects: [], sharedXP: 0, nextLevel: 0, currentPage: "", readyCount: 0, waveText: null, deadPlayers:{}};
 
 const backgroundImage = new Image();
